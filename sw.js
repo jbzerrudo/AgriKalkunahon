9207662cb109aeeb63369d05ee1a20269859bdeb
Zerrudo <215452179+jbzerrudo@users.noreply.github.com>
2026-09-15 14:32:47 +0200

    Save readings on the device, list and export them from the About card

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    VERSION is rewritten by build.js from a hash of the sources, so every build gets a new cache name.
    Required Notice: Copyright 2026 Jef Zerrudo (https://github.com/jbzerrudo/AgriKalkunahon)
    PolyForm Noncommercial License 1.0.0 */
-const VERSION = 'agrikalkunahon-71eb55d7';
+const VERSION = 'agrikalkunahon-19ceaa1b';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', e => {
